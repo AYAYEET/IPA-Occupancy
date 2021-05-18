b5e7006ceb68a0015694133222110de1e1c71871
Jaden Scheidegger <jadenvidz@gmail.com>
2021-05-18 18:50:45 +0200

    IBOutlets erstellt

--- IPAOccupancy/RegisterViewController.swift
+++ IPAOccupancy/RegisterViewController.swift
@@ -10,6 +10,14 @@ import UIKit
 
 class RegisterViewController: UIViewController {
 
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var passwordReTextField: UITextField!
+    @IBOutlet weak var clubPicker: UIPickerView!
+    @IBOutlet weak var selectClubLabel: UILabel!
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var passwordLabel: UILabel!
+    @IBOutlet weak var registerButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -17,14 +25,8 @@ class RegisterViewController: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func registerPressed(_ sender: Any) {
     }
-    */
+    
 
 }
