b3c1841280f638191fe27d6e79e5b957d06ea02d
Jaden Scheidegger <jadenvidz@gmail.com>
2021-05-18 15:07:12 +0200

    Creation of LoginModel

---
+++ IPAOccupancy/LoginModel.swift
@@ -0,0 +1,16 @@
+//
+//  LoginModel.swift
+//  IPAOccupancy
+//
+//  Created by Scheidegger, Jaden on 18.05.21.
+//  Copyright © 2021 SAP. All rights reserved.
+//
+
+import SAPFoundation
+import SAPOData
+import UIKit
+import SAPFiori
+
+struct LoginModel {
+    
+}
