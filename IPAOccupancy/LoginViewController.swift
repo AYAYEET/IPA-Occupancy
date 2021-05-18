9907b6e63dd823b326f454f2962d22bc744e5252
Jaden Scheidegger <jadenvidz@gmail.com>
2021-05-18 15:03:49 +0200

    Styling of some elements

--- IPAOccupancy/LoginViewController.swift
+++ IPAOccupancy/LoginViewController.swift
@@ -30,4 +30,35 @@ class LoginViewController: UIViewController {
     @IBAction func registerPressed(_ sender: UIButton) {
     }
     
+    // MARK: - Navigation
+    //navigation preparation
+    override func prepare(for segue: UIStoryboardSegue, sender _: Any?) {
+        switch segue.identifier {
+        case "loginSegue":
+        print()
+        case "registerSegue":
+            print()
+        default:
+            print("no segue")
+        }
+    }
+    
+    //MARK: - Button Styling (Mostly done in Storyboard)
+        //Changes to the design of Button
+        fileprivate func configureButtons() {
+            loginButton.layer.cornerRadius = 5
+            registerButton.layer.cornerRadius = 5
+        }
+    
+    //MARK: - Textfield styling (Mostly done in Storyboard)
+    fileprivate func configureTextFields() {
+        usernameTextField.layer.borderWidth = 1.0
+        passwordTextField.layer.borderWidth = 1.0
+        usernameTextField.layer.cornerRadius = 5
+        passwordTextField.layer.cornerRadius = 5
+        //color choice is based on fiori guidelines
+        usernameTextField.layer.borderColor = UIColor.preferredFioriColor(forStyle: .primary9).cgColor
+        passwordTextField.layer.borderColor = UIColor.preferredFioriColor(forStyle: .primary9).cgColor
+    }
+    
 }
