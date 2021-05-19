d4a9d806e22b49d5f114d7e3ca65d8e312eb0508
Jaden Scheidegger <jadenvidz@gmail.com>
2021-05-19 08:09:45 +0200

    Creation TabBarVC

---
+++ IPAOccupancy/TabBarViewController.swift
@@ -0,0 +1,30 @@
+//
+//  TabBarViewController.swift
+//  IPAOccupancy
+//
+//  Created by Scheidegger, Jaden on 19.05.21.
+//  Copyright © 2021 SAP. All rights reserved.
+//
+
+import UIKit
+
+class TabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
